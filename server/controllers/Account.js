bab616c8d1d76fc43a92a71ed9b6f5f62fc8c926
Jamie Zheng <jamiezheng8653@gmail.com>
2026-04-05 22:29:44 -0400

    finished domomaker B. throws error if on /maker

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -10,6 +10,7 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
+    req.session.destroy();
     return res.redirect('/');
 };
 
@@ -26,6 +27,9 @@ const login = (req, res) => {
         if (err || !account){
             return res.status(401).json({error: 'Wrong username or password!'});
         }
+
+        req.session.account = Account.toAPI(account);
+
         return res.json({redirect: '/maker'});
     });
 
@@ -48,6 +52,7 @@ const signup = async (req, res) => {
         const hash = await Account.generateHash(pass);
         const newAccount = new Account({username, password: hash});
         await newAccount.save();
+        req.session.account = Account.toAPI(newAccount);
         return res.json({redirect: '/maker'});
 
     }
